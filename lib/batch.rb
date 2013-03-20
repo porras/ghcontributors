a3aab55c6ffecf50156b364e305afbb2bc0e51f7
Sergio Gil Pérez de la Manga <sgilperez@gmail.com>
2013-03-20 08:56:01 +0100

    Add error traces to batch updates

--- lib/batch.rb
+++ lib/batch.rb
@@ -2,7 +2,12 @@ class Batch < Struct.new(:batch)
   def update
     puts "Updating batch ##{batch}"
     DB.view('ghcontributors/batches', :key => batch, :include_docs => true)['rows'].each do |row|
-      Repo.new(row['doc']['name'], row['doc']).update
+      begin
+        Repo.new(row['doc']['name'], row['doc']).update
+      rescue Exception => e
+        puts "Updating repo '#{row['doc']['name']}' failed with message #{e.message}"
+      end
     end
+    puts "Done with batch ##{batch}"
   end
 end
