36f20b0f3b22529401ebae914fd23ded1de5c2db
Sergio Gil Pérez de la Manga <sgilperez@gmail.com>
2013-03-31 20:34:10 +0200

    Optimize batch updates using bulk_save

--- lib/batch.rb
+++ lib/batch.rb
@@ -3,11 +3,13 @@ class Batch < Struct.new(:batch)
     OUT.puts "Updating batch ##{batch}"
     DB.view('ghcontributors/batches', :key => batch, :include_docs => true)['rows'].each do |row|
       begin
-        Repo.new(row['doc']['name'], row['doc']).update
+        Repo.new(row['doc']['name'], row['doc']).update(:bulk)
+        @updated = true
       rescue Exception => e
         OUT.puts "Updating repo '#{row['doc']['name']}' failed with message #{e.message}"
       end
     end
+    DB.bulk_save if @updated
     OUT.puts "Done with batch ##{batch}"
   end
 end
