afb35149824d2628da31ff0aa0aa99ee17e35fa6
Sergio Gil Pérez de la Manga <sgilperez@gmail.com>
2013-05-11 15:00:10 +0200

    Real name is only requested on adding

--- test/acceptance/case_sensitiveness_test.rb
+++ test/acceptance/case_sensitiveness_test.rb
@@ -14,26 +14,4 @@ class CaseSensitivenessTest < AcceptanceTest
     
     assert page.has_content?('rails/rails 1 commit')
   end
-  
-  def test_renaming
-    stub_github_api('rails/rails', porras: 1)
-    
-    visit '/'
-    
-    fill_in 'repo', with: 'rails/rails'
-    click_button 'Add'
-    
-    visit '/porras'
-    
-    assert page.has_content?('rails/rails 1 commit')
-    
-    stub_github_api_repo('rails/rails', full_name: 'rails/Rails')
-    stub_github_api_contributors('rails/Rails', porras: 1)
-    
-    update_all_repos
-    
-    visit '/porras'
-    
-    assert page.has_content?('rails/Rails 1 commit')
-  end
 end
